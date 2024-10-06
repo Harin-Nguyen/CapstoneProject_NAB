ae79f834c2dfbb041b00c74a3bbfc329d7e8cb80
ahnie <anhpham0397@gmail.com>
2024-10-06 17:41:05 +0700

    <commit_message>

--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -1,22 +1,34 @@
-class loginPage{
-    visit() {
-        cy.visit('http://localhost:3000/login');
-      }
-    
-      fillEmail(email) {
-        cy.get("input[placeholder='Enter email']").type(email);
-      }
-    
-      fillPassword(password) {
-        cy.get("input[placeholder='Enter password']").type(password);
-      }
-    
-      submit() {
-        cy.get(".btn.btn-primary").click(); 
-      }
-    
-      validateLoginSuccess() {
-        cy.url().should('include', 'http://localhost:3000/');
-      }
+// Tạo 1 class để loginPage:
+class loginPage {
+  visit() {
+    // Sử dụng Cypress để mở url trang web:
+    cy.visit('http://localhost:3000/login');
+  }
+
+  // Điền email vào trường email:
+  fillEmail(email) {
+    // Tìm trường nhập email bằng placeholder và nhập giá trị email
+    cy.get("input[placeholder='Enter email']").type(email);
+  }
+
+  // Điền mật khẩu vào trường nhập password:
+  fillPassword(password) {
+    // Tìm trường nhập mật khẩu bằng placeholder và nhập giá trị mật khẩu:
+    cy.get("input[placeholder='Enter password']").type(password);
+  }
+
+  // Click vào nút đăng nhập:
+  submit() {
+    // Tìm nút đăng nhập bằng class và click:
+    cy.get(".btn.btn-primary").click();
+  }
+
+  // Xác nhận việc đăng nhập thành công:
+  validateLoginSuccess() {
+    // Kiểm tra URL hiện tại có chứa đường dẫn chính xác sau khi đăng nhập ko:
+    cy.url().should('eq', 'http://localhost:3000/');
+  }
 }
+
+// Xuất class loginPage để có thể sử dụng:
 export default loginPage;
