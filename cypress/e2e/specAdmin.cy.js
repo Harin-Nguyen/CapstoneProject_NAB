33e6f6be757b9b70cd8eb3b776ae798bea6097bb
huyennguyen230103@gmail.com <huyennguyen230103@gmail.com>
2024-10-15 13:01:50 +0700

    dd

--- cypress/e2e/specAdmin.cy.js
+++ cypress/e2e/specAdmin.cy.js
@@ -6,7 +6,7 @@ import OrderManagement from '../pageObjects/OrderManagement';
 describe('E2E Tests for E-commerce Application', () => {
   const adminLogin = new AdminLogin();
   const dashboard = new AdminDashboardPage();
-  const product = new ProductManagement();
+  //const product = new ProductManagement();
   const order = new OrderManagement();
 
 
@@ -25,16 +25,16 @@ describe('E2E Tests for E-commerce Application', () => {
       });
     });
 
-    const loginAdmin = () => {
-      cy.fixture('userData').then((admin) => {
-        const adminInfo = admin.adminInfo;
-        adminLogin.visit();
-        adminLogin.fillEmail(adminInfo.email);
-        adminLogin.fillPassword(adminInfo.password);
-        adminLogin.submit();
-        adminLogin.validateLoginSuccess();
-      });
-    };
+    // const loginAdmin = () => {
+    //   cy.fixture('userData').then((admin) => {
+    //     const adminInfo = admin.adminInfo;
+    //     adminLogin.visit();
+    //     adminLogin.fillEmail(adminInfo.email);
+    //     adminLogin.fillPassword(adminInfo.password);
+    //     adminLogin.submit();
+    //     adminLogin.validateLoginSuccess();
+    //   });
+    // };
 
       it('should find a paid order and click details', () => {
         dashboard.openAdminDropdown();
@@ -42,24 +42,24 @@ describe('E2E Tests for E-commerce Application', () => {
 
         order.findAndClickRandomPaidOrder();
         order.markAsDelivered();
-    });
+      });
 
-      it('Create and delete a product', () => {
-        cy.visit('http://localhost:3000/login')
-        loginAdmin();
-        dashboard.openAdminDropdown();
-        dashboard.navigateToProducts();
+      // it('Create and delete a product', () => {
+      //   //cy.visit('http://localhost:3000/login')
+      //   loginAdmin();
+      //   dashboard.openAdminDropdown();
+      //   dashboard.navigateToProducts();
     
-        cy.fixture('userData').then((data) => {
-          const newProduct = data.newProduct; // Lấy dữ liệu sản phẩm mới từ fixture
-          const deleteProduct = data.deleteProduct; // Lấy dữ liệu sản phẩm để xóa từ fixture
+      //   cy.fixture('userData').then((data) => {
+      //     const newProduct = data.newProduct; // Lấy dữ liệu sản phẩm mới từ fixture
+      //     const deleteProduct = data.deleteProduct; // Lấy dữ liệu sản phẩm để xóa từ fixture
     
-          product.createProduct(newProduct); // Tạo sản phẩm mới
-          product.deleteProduct(deleteProduct.name); // Xóa sản phẩm
-          product.searchProduct(newProduct.name);
-          product.visitProduct(newProduct.name);
-        });
-    });
+      //     product.createProduct(newProduct); // Tạo sản phẩm mới
+      //     product.deleteProduct(deleteProduct.name); // Xóa sản phẩm
+      //     product.searchProduct(newProduct.name);
+      //     product.visitProduct(newProduct.name);
+      //   });
+      //});
   });
 
 });
